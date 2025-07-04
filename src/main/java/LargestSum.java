f0f893f3791eef17b36e5942ae6a27f2f8a603c9
Omar Hamdan <omarhamdan500@gmail.com>
2025-07-04 17:37:04 +0000

    Completed Coding Challenge: Largest Sum

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -4,11 +4,31 @@ public class LargestSum {
     /**
      * Get the largest possible sum that can be obtained from a pair of values in the list.
      * A number can't be added to itself, unless there are duplicates.
-     * 
      * @param nums a list of ints.
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums) {
-        return 0;
+        if (nums == null || nums.isEmpty()) {
+            return 0;
+        }
+        
+        if (nums.size() == 1) {
+            return nums.get(0);
+        }
+        
+        // Find the two largest numbers in the list
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        
+        for (int num : nums) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+        
+        return largest + secondLargest;
     }
 }
